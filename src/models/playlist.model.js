7d369fd38a34f0b569267740a52d254edabe95c3
adityajoshi14701 <adityajoshi14072001@google.com>
2024-08-08 19:28:00 +0530

    added tweet , like,playlist,comment models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+        required: true,
+      },
+    ],
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+export const Playlist = mongoose.model("Playlist", playlistSchema);
