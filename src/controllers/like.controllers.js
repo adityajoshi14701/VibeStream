be19719a6c5b9c5d7d016c78831d961f060463f0
adityajoshi14701 <adityajoshi14072001@google.com>
2024-08-09 17:39:26 +0530

    Added healcheck ,like,tweet controllers

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -5,22 +5,95 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
+  // check if video id is valid or not
+  // find whether the video is liked by the user or not
+  // if liked then toggle and vice versa
+  // according to that increment and decrement the number of likes of the video
+
   const { videoId } = req.params;
   //TODO: toggle like on video
+  if (!isValidObjectId(videoId)) throw new ApiError("400", "Invalid Videos Id");
+
+  const like = await Like.findOne({ video: videoId, likedBy: req.user._id });
+  if (like) {
+    await Like.delete();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Like Removed Successfully"));
+  }
+  await Like.create({ video: videoId, likedBy: req.user._id });
+  return res
+    .status(200)
+    .json(new ApiResponse("200", {}, "Liked the video succcessfully!!"));
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
+  // get user id and comment id
+  // verify them if they exist or not
+  // check if the comment is liked or not already
+  // return the response
   const { commentId } = req.params;
+  if (!isValidObjectId(commentId))
+    throw new ApiError("400", "Invalid Comment Id");
+  const comment = Like.findOne({
+    comment: commentId,
+    likedBy: req.params.user._id,
+  });
+  if (comment) {
+    await Like.delete();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Like Removed Successfully"));
+  }
+  await Like.create({ comment: commentId, likedBy: req.user._id });
+  return res
+    .status(200)
+    .json(new ApiResponse("200", {}, "Liked the video succcessfully!!"));
+
   //TODO: toggle like on comment
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
   const { tweetId } = req.params;
+  if (!isValidObjectId(tweetId))
+    throw new ApiError("400", "Invalid Comment Id");
+  const tweet = Like.findOne({
+    tweet: tweetId,
+    likedBy: req.params.user._id,
+  });
+  if (tweet) {
+    await Like.delete();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Like Removed Successfully"));
+  }
+  await Like.create({ tweet: tweetId, likedBy: req.user._id });
+  return res
+    .status(200)
+    .json(new ApiResponse("200", {}, "Liked the video succcessfully!!"));
   //TODO: toggle like on tweet
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
   //TODO: get all liked videos
+  // get user id
+  // after fetch all the liked content by user
+  // filter out the videos form content
+  const userId = req.user._id;
+  if (!isValidObjectId(userId)) throw new ApiError("400", "Invalid user ");
+  const content = Like.find({ likedBy: userId });
+  content
+    .then((likes) => {
+      const videoLikes = likes.filter((like) => like.video);
+      res.status("200", { videoLikes }, "All liked videos fetched ");
+      console.log(videoLikes);
+    })
+    .catch((err) => {
+      throw new ApiError("401", "Something went wrong", err);
+    });
 });
 
 export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
--- src/controllers/tweet.controllers.js
+++ src/controllers/tweet.controllers.js
@@ -4,21 +4,94 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { response } from "express";
 
 const createTweet = asyncHandler(async (req, res) => {
+  // get user id and content
+  // validate the user
+  // create a tweet object
+  // save it in dbms
+  // return response
+
   //TODO: create tweet
+  const { content } = req.body;
+  const owner = req.user._id;
+  if (!isValidObjectId(owner)) throw new ApiError("400", "Invalid user id");
+
+  await Tweet.create({ owner: owner, conent: content })
+    .then((tweet) => {
+      console.log(tweet);
+      res
+        .status(200)
+        .json(new ApiResponse("200", { tweet }, "Tweet created successfully"));
+    })
+    .catch((err) => {
+      throw new ApiError("401", "Error while creating tweet", err);
+    });
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
   // TODO: get user tweets
+  // get user
+  // find user in the tweet model
+  // check if there is ny tweet by user
+  // if yes retrun the tweets
+  const userId = req.params.user._id;
+  if (!isValidObjectId(owner)) throw new ApiError("400", "Invalid user id");
+  const response = Tweet.find({ owner: userId });
+  if (!response)
+    return res.status(404).json(new ApiResponse(404, {}, "User doesn't exist"));
+  const tweets = response.map((tweet) => tweet.content);
+  if (!tweets)
+    return res
+      .status(404)
+      .json(new ApiResponse(404, {}, "User doesn't have any tweets"));
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { tweets }, "Twwets fetched succesfully"));
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
+  const tweetId = req.parama.tweetId;
+  if (!tweetId) throw new ApiError(400, "This Tweet doesn't exist");
+  const newContent = req.body;
+  await Tweet.findByIdAndUpdate(
+    tweetId,
+    {
+      $set: {
+        content: newContent,
+      },
+    },
+    { new: true },
+  )
+    .then((tweet) => {
+      return res
+        .status(200)
+        .json(new ApiResponse(201, { tweet }, "Tweet Updated succeessfully"));
+    })
+    .catch((err) => {
+      return res.status(500).json(new ApiError(500, "Errorin updating file"));
+    });
+
   //TODO: update tweet
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
   //TODO: delete tweet
+  const tweetId = req.parama.tweetId;
+  if (!tweetId) throw new ApiError(400, "This Tweet doesn't exist");
+
+  try {
+    await Tweet.findByIdAndDelete(tweetId, (err, response) => {
+      if (err) console.log(err);
+      else {
+        console.log("Deleted Successful!!", response);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
