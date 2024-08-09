ea54b7601fb5e9199aa71ca26ab7f2fc42fedfa4
adityajoshi14701 <adityajoshi14072001@google.com>
2024-08-09 11:25:57 +0530

    added routes and contollers

--- src/app.js
+++ src/app.js
@@ -1,31 +1,55 @@
-import express from 'express'
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
-import e from 'cors';
-const app = express()
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import e from "cors";
+const app = express();
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true,
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
 
 // the configuration limits the size of the incoming json object/file
-app.use(express.json({
-    limit:"16kb",
-}))
+app.use(
+  express.json({
+    limit: "16kb",
+  }),
+);
 
 //to configure url encoding , extended enables us to give objects within objects
-app.use(express.urlencoded({
-    extended:true,
-    limit:'16 kb',
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16 kb",
+  }),
+);
 
-//static configuration : 
-app.use(express.static("public"))
-app.use(cookieParser())
+//static configuration :
+app.use(express.static("public"));
+app.use(cookieParser());
 
-import userRouter from "./routes/user.routes.js"
+//routes import
+import userRouter from "./routes/user.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
-app.use('/api/v1/users',userRouter);
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
-export {app};
+export { app };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,20 @@
 import { Router } from "express";
 import {
-  changeCurrentPassword,
-  getCurrentUser,
-  getUserChannelProfile,
-  getWatchHistory,
   loginUser,
   logoutUser,
-  refreshAccessToken,
   registerUser,
-  updateAccountDetails,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
   updateUserAvatar,
   updateUserCoverImage,
-} from "../controllers/user.controllers.js";
+  getUserChannelProfile,
+  getWatchHistory,
+  updateAccountDetails,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const router = Router();
 
 router.route("/register").post(
@@ -31,18 +32,22 @@ router.route("/register").post(
 );
 
 router.route("/login").post(loginUser);
+
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
 router
   .route("/avatar")
-  .patch(verifyJWT, upload.sinlge("avatar"), updateUserAvatar);
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
   .route("/cover-image")
   .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
-router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
+
 export default router;
